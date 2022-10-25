b64d524b0cf0b57a4c89e00ab14668a18d112d0e
thiago <ts_muniz@outlook.com>
2022-10-25 13:07:05 -0300

    fix: configurações do build do projeto

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })]
+  plugins: 
+  [
+    react(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+     })
+  ]
 })
